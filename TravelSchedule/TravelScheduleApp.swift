9a3b91262a98918f79ccc85aa688f1c7450d3a35
andmakalkin <mkdir.gammak@yandex.ru>
2026-07-15 16:42:19 +0300

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Андрей Макалкин on 15.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
